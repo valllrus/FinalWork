e9a1cb81736140df2f5ab638be2a59e48a2d9693
VOsipov <legoman380@mail.ru>
2023-08-15 10:42:47 +0300

    Task.cs reworked

--- Work/Task.cs
+++ Work/Task.cs
@@ -1,4 +1,4 @@
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array1 = new string[5] {"888", "41", "hello", "student", "sempai"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
